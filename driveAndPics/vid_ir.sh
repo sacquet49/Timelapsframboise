b7536fd6ecb60153a7ca42a3e3ba80272433ca70
DUCLOS Maxime <mduclos@sodifrance.fr>
2024-03-23 17:25:22 +0100

    Ajout d'une commande pour l'infrarouge optimise

--- driveAndPics/vid_ir.sh
+++ driveAndPics/vid_ir.sh
@@ -11,7 +11,11 @@ DATEPHOTO=$(date +"%Y-%m-%d_%H%M%S")
 # Prend une video, envoie drive et supprime en local
 echo $(date +"%Y-%m-%d_%H:%M:%S")" : INFO : Prise de vue $REPERTOIRESTOCKAGE/$DATEPHOTO.mkv" >> $LOG
 
-rpicam-vid --framerate 25 --profile baseline -t 100000 -o $REPERTOIRESTOCKAGE/$DATEPHOTO.h264 --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json --save-pts $REPERTOIRESTOCKAGE/$DATEPHOTO.txt --nopreview
+rpicam-vid --framerate 25 --profile baseline -t 100000 -o $REPERTOIRESTOCKAGE/$DATEPHOTO.h264 --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json \
+--save-pts $REPERTOIRESTOCKAGE/$DATEPHOTO.txt --nopreview
+# Avec des paramètres optimisé mais il faut avoir des led IR pour que le capteur soit sensible dans le noir
+# rpicam-vid -o $REPERTOIRESTOCKAGE/$DATEPHOTO.h264 --framerate 25 --profile baseline -t 100000 --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json \
+# -t 1000000 --width 1920 --height 1080 --saturation 0 --ev 1 --awb cloudy --save-pts $REPERTOIRESTOCKAGE/$DATEPHOTO.txt --nopreview
 mkvmerge -o $REPERTOIRESTOCKAGE/$DATEPHOTO.mkv --timecodes 0:$REPERTOIRESTOCKAGE/$DATEPHOTO.txt $REPERTOIRESTOCKAGE/$DATEPHOTO.h264
 
 rclone copy $REPERTOIRESTOCKAGE/$DATEPHOTO.mkv drive:Catcam/Pi2
