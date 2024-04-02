b52ab4f9786ea8270cc9c64d0c3fb0c8f217ec53
sacquet <sacquet@>
2024-04-02 20:25:41 +0200

    Ajout de la doc sur le crontab

--- driveAndPics/timelaps.sh
+++ driveAndPics/timelaps.sh
@@ -5,6 +5,10 @@ LOG="/var/log/picam/timelaps.log"
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 REPERTOIRESTOCKAGE=$SCRIPT_DIR
 NBPHOTO=10
+# 58 minutes
+# TEMPCAPTURE=3480000
+# 2 minutes valeur de test
+TEMPCAPTURE=120000
 
 # Prend un nombre de prise de vue défini en parametrage
 for numero in `seq 1 $NBPHOTO`
@@ -17,9 +21,9 @@ echo $(date +"%Y-%m-%d_%H:%M:%S")" : INFO : Début prise de vue" >> $LOG
   # Prend une photo, envoie drive et supprime en local
   echo $(date +"%Y-%m-%d_%H:%M:%S")" : INFO : Prise de vue $REPERTOIRESTOCKAGE/$DATEPHOTO.jpg" >> $LOG
 
-  rpicam-still -q 90 -t 3480000 --datetime --timelapse 2000 --width 3280 --height 2464 --nopreview
+  rpicam-still -q 90 -t $TEMPCAPTURE -o $REPERTOIRESTOCKAGE/$(date +"%Y-%m-%d_%H:%M:%S").jpg --timelapse 2000 --width 3280 --height 2464 --nopreview
 
-  ffmpeg -r 30 -f image2 -pattern_type glob -i '*.jpg' -s 1920x1080 -vcodec libx264 $REPERTOIRESTOCKAGE/$DATEPHOTO.mp4
+  ffmpeg -r 25 -f image2 -pattern_type glob -i "$REPERTOIRESTOCKAGE/*.jpg" -s 1920x1080 -vcodec libx264 $REPERTOIRESTOCKAGE/$DATEPHOTO.mp4
   rclone copy $REPERTOIRESTOCKAGE/$DATEPHOTO.mp4 drive:Catcam/Pi3
 
   rm $REPERTOIRESTOCKAGE/*.jpg
