00d755833d94dc6224d6161f2e6d99c78494b246
sacquet <sacquet@>
2024-04-02 20:42:11 +0200

    Ajout de la doc sur le crontab

--- driveAndPics/driveClean.sh
+++ driveAndPics/driveClean.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo $(date +"%Y-%m-%d_%H:%M:%S")" : INFO : Début suppression image du drive" >> $LOG
 
-rclone --min-age 24h delete drive:Catcam/Pi2
-rclone --min-age 24h delete drive:Catcam/Pi3
+rclone --min-age 4h delete drive:Catcam/Pi2
+rclone --min-age 4h delete drive:Catcam/Pi3
 
 echo $(date +"%Y-%m-%d_%H:%M:%S")" : INFO : Fin suppression image du drive" >> $LOG
